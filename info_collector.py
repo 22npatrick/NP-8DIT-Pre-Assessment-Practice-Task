45ab80fe38ce416b53d89630f86dcfa715cc3edf
22npatrick <22npatrick@wakatipu.school.nz>
2026-04-19 15:29:50 +1200

    Created the frames

--- info_collector.py
+++ info_collector.py
@@ -11,63 +11,72 @@ class InfoCollectorGUI:
         self.v = IntVar()
         self.v.set(None)
 
+        #Frame 1
         self.lb1 = Label(self.f1, text = "Collecting Person Data", bg="pink")
         self.lb1.grid(row = 0, column = 0)
 
-        self.bt1 = Button(self.f1, text = "Show all")
-        self.bt1.grid()
+        self.bt1 = Button(self.f1, text = "Show all",  command=lambda: self.switch_frame(1))
+        self.bt1.grid(row = 0, column = 2)
 
         self.lb2 = Label(self.f1, text = "First Name")
-        self.lb2.grid()
+        self.lb2.grid(row = 2, column = 0)
 
         self.e1 = Entry(self.f1)
-        self.e1.grid()
+        self.e1.grid(row = 2, column = 2)
 
         self.lb3 = Label(self.f1, text = "Age")
-        self.lb3.grid()
+        self.lb3.grid(row = 4, column = 0)
 
         self.e2 = Entry(self.f1)
-        self.e2.grid()
+        self.e2.grid(row = 4, column = 2)
 
         self.lb4 = Label(self.f1, text = "Do you have a mobile phone")
-        self.lb4.grid()
+        self.lb4.grid(row = 6, column = 0)
         
         self.rb1 = Radiobutton(self.f1, variable = self.v, value = 0, text = "no")
-        self.rb1.grid()
+        self.rb1.grid(row = 6, column = 2)
         
         self.rb2 = Radiobutton(self.f1, variable = self.v, value = 1, text = "yes")
-        self.rb2.grid()
+        self.rb2.grid(row = 8, column = 2)
 
-        self.lb5 = Label(self.f2, text = "Displaying Person Data", bg="pink")
+        #Frame 2
+
+        self.lb5 = Label(self.f2, text = "Displaying Person Data", bg="pink" )
         self.lb5.grid(row = 0, column = 0)
 
-        self.bt2 = Button(self.f2, text = "Add New Person")
-        self.bt2.grid()
+        self.bt2 = Button(self.f2, text = "Add New Person", command=lambda: self.switch_frame(0))
+        self.bt2.grid(row = 0, column = 2)
 
         self.lb6 = Label(self.f2, text = "First Name")
-        self.lb6.grid()
+        self.lb6.grid(row = 2, column = 0)
 
         self.lb7 = Label(self.f2, text = "First Name of persom")
-        self.lb7.grid()
+        self.lb7.grid(row = 2, column = 2)
 
         self.lb8 = Label(self.f2, text = "Age")
-        self.lb8.grid()
+        self.lb8.grid(row = 4, column = 0)
 
         self.lb9 = Label(self.f2, text = "Age of person")
-        self.lb9.grid()
+        self.lb9.grid(row = 4, column = 2)
 
         self.lb10 = Label(self.f2, text = 'Person (has)/(does not has) a mobile phone')
-        self.lb10.grid()
+        self.lb10.grid(row = 6, column = 0, columnspan = 3)
 
         self.bt3 = Button(self.f2, text = "Previous")
-        self.bt3.grid()
+        self.bt3.grid(row = 8, column = 0)
         
         self.bt4 = Button(self.f2, text = "Next")
-        self.bt4.grid()
+        self.bt4.grid(row = 8, column = 2)
 
+        #grid f1
         self.f1.grid()
-    
-
+    def switch_frame(self, amount):
+        if amount == 1:
+            self.f1.grid_forget()
+            self.f2.grid()
+        else:
+            self.f2.grid_forget()
+            self.f1.grid()
 
 
 if __name__=="__main__":
